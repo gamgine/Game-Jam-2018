e819c34a62aa93b3c1b52da72e0e4c03a0a2aa22
thomas-NC <thomas.nguyencong@ynov.com>
2018-05-26 23:20:32 +0200

    fix ui ang

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -5,33 +5,32 @@ public class UI : MonoBehaviour
 {
     public Entity pl, en;
     public Transform plPanel, enPanel;
-    float z;
+    float y;
     public float drop;
     public float dropSpeed;
     void Start()
     {
-        z = plPanel.position.z;
-        drop =z-drop;
+        y = plPanel.position.y;
+        drop =y-drop;
     }
 
     void Update()
     {
         if (!pl)
-        { if (plPanel.position.z < z){ plPanel.position += new Vector3(0.0f, 0.0f, (Time.deltaTime*dropSpeed) ); } }
+        { if (plPanel.position.y < y )     { plPanel.position += new Vector3(0f, (Time.deltaTime * dropSpeed), 0f ); } }
         else
         {
-            if (plPanel.position.z >= drop   ){
-                    plPanel.position += new Vector3(0.0f, 0.0f, -(Time.deltaTime*dropSpeed) ); }
+            if (plPanel.position.y >= drop ){ plPanel.position += new Vector3(0f, -(Time.deltaTime * dropSpeed) , 0f); }
             plPanel.Find("StatsAlly").GetComponent<Text>().text = pl.hp.ToString();
             plPanel.Find("StatsAlly2").GetComponent<Text>().text = pl.def.ToString();
             plPanel.Find("StatsAlly3").GetComponent<Text>().text = pl.deg.ToString();
         }
 
         if (!en)
-        { if (enPanel.position.z < z) { enPanel.position += new Vector3(0.0f, 0.0f, (Time.deltaTime * dropSpeed)); } }
+        { if (enPanel.position.y < y)      { enPanel.position += new Vector3(0.0f, (Time.deltaTime * dropSpeed), 0f); } }
         else
         {
-            if (enPanel.position.z >= drop) { enPanel.position += new Vector3(0.0f, 0.0f, -(Time.deltaTime * dropSpeed)); }
+            if (enPanel.position.y >= drop) { enPanel.position += new Vector3(0.0f, -(Time.deltaTime * dropSpeed), 0f); }
             enPanel.Find("StatsAlly").GetComponent<Text>().text = en.hp.ToString();
             enPanel.Find("StatsAlly2").GetComponent<Text>().text = en.def.ToString();
             enPanel.Find("StatsAlly3").GetComponent<Text>().text = en.deg.ToString();

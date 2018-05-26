7e3624bb128612a6044fc54a0696607d4678e904
gamgine <victor.fromentin95@ynov.com>
2018-05-26 14:28:54 +0200

    map block autohide

---
+++ Assets/Scripts/AutoHide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoHide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        foreach (MeshRenderer el in gameObject.GetComponentsInChildren<MeshRenderer>()) {el.enabled = false; }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

afab17f0baa94ec260699d09d2058c877d6968a5
gamgine <victor.fromentin95@ynov.com>
2018-05-26 22:53:00 +0200

    ground fix + player inter

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject select;
+	void Start () {}
+	void Update ()
+    {
+        if (GameManager.player)
+        {
+            if (Input.GetMouseButtonDown(0))//select
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+                {
+                    if (!select && hit.transform.gameObject.tag == "Player")
+                    {
+                        if (hit.transform.gameObject.GetComponent<Entity>().player) { select = hit.transform.gameObject; }
+                        else { }
+                    }
+                }
+            }
+            else if (Input.GetMouseButtonDown(1) && select)//action
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+                { select.GetComponent<Entity>().MvTo(hit.point); }
+            }
+        }
+    }
+}

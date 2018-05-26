cb3fc5281b198fc04503ce783d3b227f4f541c66
gamgine <victor.fromentin95@ynov.com>
2018-05-26 18:17:45 +0200

    tileset

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : MonoBehaviour
+{
+    public int hp, deg, def;
+    public bool player;
+    public float shootRange,minShoot;
+
+    void Start(){}
+
+    void Update(){}
+
+    void Die() { }
+
+    public void Damages(int d)
+    {
+        this.hp -= d;
+        if (this.hp < 0) { this.Die(); }
+    }
+}
